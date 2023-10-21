f1cb9c1e29de8bea1cd63646643ef3e35d2fb2e0
Sam O'Cain <samocain93@gmail.com>
2023-10-20 19:07:18 -0500

    fixed rendering issue that was occuring

--- client/src/app.js
+++ client/src/app.js
@@ -1,15 +1,19 @@
 import React from 'react';
 
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Chat from './components/Chat';
+import Join from './components/Join';
 
-import Join from './components/Join' 
-import Chat from './components/Chat'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const App = () => {
-  <Router>
-    <Route path='/' exact component={Join} />
-    <Route path='/chat' component={Chat} />
-  </Router>;
+  return (
+    <Router>
+      <Routes>
+       <Route path='/' element={<Join />} />
+        <Route path='/chat' element={<Chat />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
--- client/src/components/Join.js
+++ client/src/components/Join.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Join = () => {
   return (
     <div>
-      
+      <h1>Join</h1>
     </div>
   )
 }
