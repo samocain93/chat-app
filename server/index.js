824b972cb56753ebda819467a9cbb15931418aae
Sam O'Cain <samocain93@gmail.com>
2023-10-24 18:38:00 -0500

    fixed location search bug

--- server/index.js
+++ server/index.js
@@ -1,22 +1,19 @@
 const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
+const cors = require('cors');
+
 
 const PORT = process.env.PORT || 5000;
 
 const router = require('./router');
 
 const app = express();
+app.use(cors());
 const server = http.createServer(app);
 const io = socketio(server);
 
-io.on('connection', (socket) => {
-  console.log('We have a new connection!');
 
-  socket.on('disconnect', () => {
-    console.log('User has left');
-  });
-});
 
 app.use(router);
 
